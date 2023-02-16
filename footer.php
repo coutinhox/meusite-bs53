d0c791f5b399b10e009b69ba6e901aed528915e9
Pr. Antonio Carlos Coutinho <coutinhox@gmail.com>
2023-02-16 16:13:03 -0300

    Iniciando o projeto

---
+++ footer.php
@@ -0,0 +1,46 @@
+<footer class="bg-light text-center text-lg-start">
+  <div class="container p-4">
+    <div class="row">
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Contato</h5>
+        <p>
+          Endereço: Rua Exemplo, 123 - Cidade/Estado
+          <br>
+          Telefone: (99) 9999-9999
+          <br>
+          Email: exemplo@meusite.com
+        </p>
+      </div>
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Links Úteis</h5>
+        <ul class="list-unstyled mb-0">
+          <li>
+            <a href="pages/sobre.php" class="text-dark">Sobre</a>
+          </li>
+          <li>
+            <a href="#" class="text-dark">Termos de Uso</a>
+          </li>
+          <li>
+            <a href="#" class="text-dark">Política de Privacidade</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Redes Sociais</h5>
+        <ul class="list-unstyled mb-0">
+          <li>
+            <a href="#" class="text-dark"><i class="bi bi-facebook"></i> Facebook</a>
+          </li>
+          <li>
+            <a href="#" class="text-dark"><i class="bi bi-twitter"></i> Twitter</a>
+          </li>
+          <li>
+            <a href="#" class="text-dark"><i class="bi bi-instagram"></i> Instagram</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2023 Meu Site. Todos os direitos reservados.
+    </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">Igrejaweb</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="/aulas/bootstrap53/index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/aulas/bootstrap53/pages/suporte.php">Suporte</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/aulas/bootstrap53/pages/tutorial.php">Tutorial</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/aulas/bootstrap53/pages/sobre.php">Sobre</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Diversos
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="/aulas/bootstrap53/pages/podcasts.php">Podcast Tecnologia</a></li>
+            <li><a class="dropdown-item" href="/aulas/bootstrap53/pages/usuarios.php">Usuarios por Local</a></li>
+            <li><a class="dropdown-item" href="/aulas/bootstrap53/pages/membros.php">Membros por Local</a></li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="/aulas/bootstrap53/pages/aprenderti.php">Aprender TI</a></li>
+          </ul>
+        <li class="nav-item">
+          <a class="nav-link" href="/aulas/bootstrap53/pages/contato.php">Contato</a>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+
+</html>
