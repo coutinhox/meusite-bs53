a9a8fa5f3ca57c5411525e2077961b1f373b4475
Pr. Antonio Carlos Coutinho <coutinhox@gmail.com>
2023-02-16 16:48:36 -0300

    Incluindo as páginas do site

---
+++ pages/sobre.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="utf-8">
+  <title>Igrejaweb</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="../assets/img/favicon.png" type="image/x-icon">
+  <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+  <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <link rel="favicon" href="../assets/img/favicon.png">
+</head>
+
+<body>
+  <?php require_once('../header.php'); ?>
+  <div class="container mt-3">
+    <h1>Bem-vindo ao Sobre</h1>
+    <p>Este é o meu site. Aqui você encontrará tudo o que precisa saber sobre o meu trabalho.</p>
+  </div>
+  <?php require_once('../footer.php'); ?>
+</body>
+
+</html>
